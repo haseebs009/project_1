01487efbc5f9cbacc8cc2a2cd3f9c0009890c96e
haseebs009 <90164156+haseebs009@users.noreply.github.com>
2022-07-04 00:51:44 +0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    public class Calculate_fare
+    {
+        private string id;
+        protected double hours;
+       
+      public void sethours_id(double h, string str)
+        {
+            hours = h;
+            id = str;
+        }
+        protected void getid()
+        {
+            Console.WriteLine("The id is " + id);
+        }
+
+        
+        public double gethours()
+        {
+            return hours;
+        }
+
+        public void Display(double total)
+        {
+            getid();
+            Console.WriteLine("The garage charges are $" + total);
+        }
+        public virtual void calculatefare()
+        {
+            double total = 0f;
+            Display(total);
+        }
+
+    }
+    class bike : Calculate_fare
+    {
+       private double total;
+
+        public override void calculatefare()
+        {
+
+            total = 2.00f;
+            if (hours > 3 && hours < 24)
+            {
+                total = (float)(total + (hours - 3) * 0.5);
+            }
+            else if (hours >= 24)
+            {
+                total = 10;
+            }
+
+            Display(total);
+        }
+    }
+    class cars : Calculate_fare
+    {
+       
+        private double total;
+        public override void calculatefare()
+        {
+            total = 4.00f;
+            if (hours > 3 && hours < 24)
+            {
+                total = (float)(total + (hours - 3) * 1);
+            }
+            else if (hours >= 24)
+            {
+                total = 20;
+            }
+
+            Display(total);
+        }
+
+    }
+        class bus : Calculate_fare
+        {
+        private double total;
+        public  override void calculatefare()
+            {
+                total = 6.00f;
+                if (hours > 3 && hours < 24)
+                {
+                    total = (float)(total + (hours - 3) * 1.5);
+                }
+                else if (hours >= 24)
+                {
+                    total = 30;
+                }
+                Display(total);
+
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the vehicle type:Bike/Car/Bus");
+                string s = Console.ReadLine();
+
+                Console.WriteLine("Enter the id");
+                string id = Console.ReadLine();
+                Console.WriteLine("Enter number of hours");
+                double hr = Convert.ToDouble(Console.ReadLine());
+           
+            Calculate_fare bas;
+           
+
+          
+            if (s == "Bike")
+                {
+                bike bik = new bike();
+                bik.sethours_id(hr, id);
+                bas = bik;
+                  bas.calculatefare();
+                }
+                else if (s == "Car")
+
+                {
+                cars ca = new cars();
+                ca.sethours_id(hr, id);
+
+                bas =ca;
+                bas.calculatefare();
+            }
+
+                else if (s == "Bus")
+                {
+                bus bu = new bus();
+                bu.sethours_id(hr, id);
+                bas =bu;
+                bas.calculatefare();
+
+            }
+
+             
+        }
+        }
+
+
+    
+}
---
+++ salary slip/salary slip/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    public class salarySlip
+    {
+        private string designation;
+       
+        public void setdes(string str)
+        {
+            designation = str;
+        }
+        protected void getdes()
+        {
+            Console.WriteLine("Your designation is " + designation);
+        }
+       protected int basic = 1500;
+
+        public void Display(double total)
+        {
+            getdes();
+            Console.WriteLine("Your Salary is $" + total);
+        }
+        public virtual void calculatesalary()
+        {
+            double total = 0f;
+            Display(total);
+        }
+
+    }
+    class engineer : salarySlip
+    {
+        private double total;
+
+        public override void calculatesalary()
+        {
+            int fuel_allowance = 100;
+            int medical_allowance = 500;
+            total = basic+fuel_allowance+medical_allowance;
+            
+            Display(total);
+        }
+    }
+    class manager : salarySlip
+    {
+        private double total;
+
+        public override void calculatesalary()
+        {
+            int fuel_allowance = 250;
+            int medical_allowance = 1000;
+            total = basic + fuel_allowance + medical_allowance;
+
+            Display(total);
+        }
+    }
+
+    class analyst : salarySlip
+    {
+        private double total;
+
+        public override void calculatesalary()
+        {
+            int fuel_allowance = 150;
+            int medical_allowance = 800;
+            total = basic + fuel_allowance + medical_allowance;
+
+            Display(total);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your designation:engineer/manager/analyst");
+            string s = Console.ReadLine();
+
+         salarySlip salary;
+
+
+
+            if (s == "engineer")
+            {
+                engineer engsalary = new engineer();
+                engsalary.setdes (s);
+                salary = engsalary;
+               salary.calculatesalary();
+            }
+
+           else if (s == "manager")
+            {
+                manager managersalary = new manager();
+                managersalary.setdes(s);
+                salary = managersalary;
+                salary.calculatesalary();
+            }
+
+            else if (s == "analyst")
+
+            {
+                analyst analystsalary = new analyst();
+                analystsalary.setdes(s);
+                salary = analystsalary;
+                salary.calculatesalary();
+            }
+
+            
+        }
+    }
+
+
+
+}
